be013cbf57d7bf66385a2de7f97cb9ef2ca551e4
Aissam Hammoutane <ahammout@e3r5p2.1337.ma>
2023-07-17 19:41:28 +0100

    Update ex02

--- ex02/src/ShrubberyCreationForm.cpp
+++ ex02/src/ShrubberyCreationForm.cpp
@@ -14,6 +14,26 @@
 
 ShrubberyCreationForm::ShrubberyCreationForm()
 {
-    // INIT ALL THE CLASS ATTRIBUTES
-    A
-}  
+    // INIT ALL THE CLASS ATTRIBUTE
+    AForm();
+    std::cout << "Form: " << getName() << " default constructor called" << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(std::string n, bool _sign, int _sg, int _exg)
+{
+    AForm(n, _sign, _sg, _exg);
+    std::cout << "Form: " << getName() << " parametred constructor called" << std::endl;
+}
+
+ShrubberyCreationForm::~ShrubberyCreationForm()
+{
+    std::cout << "Form: " << getName() << " destructor called" << std::endl;
+}
+
+ShrubberyCreationForm::ShrubberyCreationForm(ShrubberyCreationForm & SRC): AForm(SRC.getName(), SRC.getSignature(), SRC.getSgrade(), SRC.getExGrade())
+{
+    std::cout << "Form: " << getName() << " copy constructor called" << std::endl;
+    *this = SRC;
+}
+
+ShrubberyCreationForm&  
