8cefee60218e9d14eae91900277548c1aa11184d
ahammout <issamhammoutan2@gmail.com>
2023-07-20 22:38:47 +0100

    Add tldrw file

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,20 +6,20 @@
 /*   By: ahammout <ahammout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 16:19:26 by ahammout          #+#    #+#             */
-/*   Updated: 2023/07/20 18:57:22 by ahammout         ###   ########.fr       */
+/*   Updated: 2023/07/20 20:49:22 by ahammout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/Bureaucrat.hpp"
-
-///@note
-    
+#include "../includes/Bureaucrat.hpp
+#include "../includes/AForm.hpp"
+#include "../includes/ShrubberyCreationForm.hpp"
 
 int main(void)
 {
     ///@note Test the abstract class ShrubberyCreationForm.
     AForm   *form;
     
-    // std::cout << "The name of the form: "<< form.getName() << std::endl;
+    form = ShrubberyCreationForm;
+    std::cout << "The name of the form: "<< form->getName() << std::endl;
     return (0);
 }
